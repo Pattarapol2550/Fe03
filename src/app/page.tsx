33bd70084f24e0a87ffacf1190d63d7a42fb080a
Pattarapol Jamsai <6833212121@student.chula.ac.th>
2026-03-11 11:13:25 +0700

    final2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Banner from "@/components/Banner";
-import Card from "@/components/Card.";
+import Card from "@/components/Card";
 export default function Home() {
   return (
     
@@ -9,9 +9,7 @@ export default function Home() {
         <Banner/>
         <div style={{margin:"20px" , display : "flex" ,flexDirection : "row",flexWrap:"wrap" , justifyContent:"space-around" ,alignContent:"space-around"}}>
           <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+          
         </div>
         
        </main>
